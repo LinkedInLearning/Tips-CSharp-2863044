8551e5f541065e7bc98ff53bc6cbeb196843aaeb
walt-instructor <staffauthor@outlook.com>
2020-08-18 20:27:32 -0700

    cleanup code

--- source/TipsConsole/Examples.cs
+++ source/TipsConsole/Examples.cs
@@ -7,13 +7,15 @@ using System.Text;
 namespace TipsConsole
 {
 	internal class Examples
-	{ 
+	{
 		public void ShowExample()
 		{
 
 			// the underscore can serve as the discards placeholder
-			// think of discards as a temporary, dummy variables that are intentionally unused in application code. 
-			// Discards are equivalent to unassigned variables; they do not have a value.
+			// think of discards as a temporary, dummy variables 
+			// that are intentionally unused in application code. 
+			// Discards are equivalent to unassigned variables; 
+			// they do not have a value.
 
 
 			// assign an expression to a discard.
@@ -23,17 +25,17 @@ namespace TipsConsole
 
 			// available for
 			// Calls to methods with out parameters.
-			// pattern matching with "is" and "switch".
 			// tuple and object deconstruction
+			// pattern matching with "is" and "switch".
 
-			//DiscardOutParam();
-			Deconstruct();
+			DiscardOutParam();
+			// Deconstruct();
 
 		}
 		public void DiscardOutParam()
 		{
 
-			var inputs = new List<string>{ "123", "234", "abc", "def" };
+			var inputs = new List<string> { "123", "234", "abc", "def" };
 
 			foreach (string input in inputs)
 			{
@@ -42,12 +44,13 @@ namespace TipsConsole
 				else
 					Console.WriteLine($"'{input}': invalid");
 			}
-			
 		}
 
 		public void Deconstruct()
 		{
-			var robot = new Robot { Name = "X632", HasWheels = true, SensorCount = 12 };
+			var robot = new Robot { Name = "X632", 
+															HasWheels = true, 
+															SensorCount = 12 };
 			var (a, b, c) = robot.GetStats();
 
 			var (x, y, _) = robot.GetStats();
@@ -58,7 +61,7 @@ namespace TipsConsole
 	public class Robot
 	{
 		public string Name { get; set; }
-		public bool HasWheels{ get; set; }
+		public bool HasWheels { get; set; }
 		public int SensorCount { get; set; }
 		public (string, bool, int) GetStats()
 		{
